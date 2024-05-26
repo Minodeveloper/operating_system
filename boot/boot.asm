14db14310f2a87e411e610f4e8c95a87c1fa4504
madhureshChoudhary (minoshi) <minoshideveloper8@gmail.com>
2024-05-26 12:36:41 +0530

    learning about GTD
    global descriptor table

--- boot/boot.asm
+++ boot/boot.asm
@@ -1,59 +1,74 @@
-[org 0x00]
-[bits 16]
-
-section code
-
-.init:
-    mov eax, 0x7c0
-    mov ds,  eax
-    mov eax, 0xb800
-    mov es,  eax
-    mov eax, 0;
-    mov ebx, 0;
-    mov ecx, 0;
-    mov dl,  0;
-
-
-.clear:
-    mov byte [es:eax], 0;
-    inc eax
-    mov byte [es:eax], 0x30;
-    inc eax
-
-    cmp eax, 2 * 25 * 80
-    jl .clear
-
-mov eax, text
-mov ecx, 3 * 2 * 80
-push .end
-call .print
-
-
-.end:
-    jmp $
-
-.print:
+.print_main:
     mov dl, byte [eax + ebx]
     
     cmp dl, 0
     je .print_end
-
-    mov byte[es:ecx], dl
+    
+    mov byte [es:ecx], dl
 
     inc ebx
     inc ecx
     inc ecx
-
-    jmp .print
+    
+    jmp .print_main
 
 .print_end:
     ret
 
-text: db "Hello, World!",0x0a,0
-text1: db "This is another text", 0
-            
+.switch:
+    cli ; Turh off the interrupts
+    lgdt [gdt_descriptor] ; Load the GDT Table
 
-    times 510 - ($-$$) db 0x00;
+    mov eax, cr0
+    or eax, 0x1
+    mov cr0, eax ; Make the switch
 
-    db 0x55
-    db 0xaa
+    jmp protected_start
+
+welcome: db 'Welcome to SaphireOS.', 0
+
+[bits 32]
+protected_start:
+    mov ax, data_seg
+    mov ds, ax
+    mov ss, ax
+    mov es, ax
+    mov fs, ax
+    mov gs, ax
+
+    ; Update the stack pointer
+    mov ebp, 0x90000
+    mov esp, ebp
+
+    jmp $
+
+gdt_begin:
+gdt_null_descriptor:
+    dd 0x00
+    dd 0x00
+gdt_code_seg:
+    dw 0xffff
+    dw 0x00
+    db 0x00
+    db 10011010b
+    db 11001111b
+    db 0x00
+gdt_data_seg:
+    dw 0xffff
+    dw 0x00
+    db 0x00
+    db 10010010b
+    db 11001111b
+    db 0x00
+gdt_end:
+gdt_descriptor:
+    dw gdt_end - gdt_begin - 1
+    dd gdt_begin
+
+code_seg equ gdt_code_seg - gdt_begin
+data_seg equ gdt_data_seg - gdt_begin
+
+times 510 - ($ - $$) db 0x00 ; Pads the file with 0s, making the file the right size
+
+db 0x55
+db 0xaa
